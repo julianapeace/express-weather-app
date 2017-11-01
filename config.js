d12d8e9a2f38286d94a3eec799ce940fd8771f57
julianapeace <chancecordelia@gmail.com>
2017-11-01 17:34:28 -0500

    initial commit

--- config.js
+++ config.js
@@ -2,10 +2,6 @@ const importEnv = require('import-env')
 
 const config = importEnv(
     {
-        name: 'PORT',
-        default: 4444
-    },
-    {
         name: 'GOOGLE_API_KEY',
         alias: 'GKEY',
         required: true
